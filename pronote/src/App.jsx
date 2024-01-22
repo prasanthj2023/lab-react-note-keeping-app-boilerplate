32d2464f43037ec83ef5917b57e6f5ab7bf6f314
prasanthj2023 <prasanth.j@kalvium.community>
2024-01-22 14:08:08 +0530

    updated

---
+++ pronote/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Editor from "./Components/Editor";
+
+function App() {
+  return <div className="App">
+    <Editor/>
+  </div>;
+}
+
+export default App;
